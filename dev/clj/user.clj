74e6e6d2639f39bd98b214e57153851667a74be0
skynet <skynet-gh@users.noreply.github.com>
2020-07-14 22:04:38 -0400

    Initial cljfx app

---
+++ dev/clj/user.clj
@@ -0,0 +1,9 @@
+(ns user
+  (:require
+    [clojure.tools.namespace.repl :refer [refresh]]))
+    ;[spring-lobby]))
+
+
+#_
+(defn ui []
+  (spring-lobby))
---
+++ src/clj/spring_lobby.clj
@@ -0,0 +1,52 @@
+(ns spring-lobby
+  (:require
+    [cljfx.api :as fx]))
+
+
+(defonce *state
+  (atom {}))
+
+(defmulti event-handler :event/type)
+
+
+(defn root-view [{{:keys []} :state}]
+  {:fx/type :stage
+   :showing true
+   :title "Alt Spring Lobby"
+   :width 300
+   :height 100
+   :scene {:fx/type :scene
+           :root {:fx/type :v-box
+                  :alignment :top-left
+                  :children [{:fx/type :menu-bar
+                              :menus
+                              [{:fx/type :menu
+                                :text "menu1"
+                                :items [{:fx/type :menu-item
+                                         :text "menu1 item1"}
+                                        {:fx/type :menu-item
+                                         :text "menu1 item2"}]}
+                               {:fx/type :menu
+                                :text "menu2"
+                                :items [{:fx/type :menu-item
+                                         :text "menu2 item1"}
+                                        {:fx/type :menu-item
+                                         :text "menu2 item2"}]}
+                               {:fx/type :menu
+                                :text "menu3"
+                                :items [{:fx/type :menu-item
+                                         :text "menu3 item1"}
+                                        {:fx/type :menu-item
+                                         :text "menu3 item2"}]}]}
+                             {:fx/type :label
+                              :text "Hello world"}]}}})
+
+(def renderer
+  (fx/create-renderer
+    :middleware (fx/wrap-map-desc (fn [state]
+                                    {:fx/type root-view
+                                     :state state}))
+    :opts {:fx.opt/map-event-handler event-handler}))
+
+
+(fx/mount-renderer *state renderer)
