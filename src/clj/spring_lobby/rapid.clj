daf31fd9467f4dfac11a29be3a5366324d13d769
skynet <skynet-gh@users.noreply.github.com>
2020-11-22 14:10:52 -0500

    Add decoding of rapid packages

--- src/clj/spring_lobby/rapid.clj
+++ src/clj/spring_lobby/rapid.clj
@@ -1,9 +1,106 @@
 (ns spring-lobby.rapid
   "Utilities for interacting with rapid and its files."
   (:require
-    [clojure.string :as string]))
+    [clojure.java.io :as io]
+    [clojure.string :as string]
+    [org.clojars.smee.binary.core :as b]
+    [spring-lobby.fs :as fs])
+  (:import
+    (java.util.zip GZIPInputStream)))
 
 
 ; https://springrts.com/wiki/Rapid
 
 
+(def md5-codec
+  (b/compile-codec
+    (b/blob :length 16)
+    ;(b/repeated :ubyte :length 16)
+    (fn [md5-string]
+      (.toByteArray
+        (java.math.BigInteger. md5-string 16))) ; TODO verify
+    (fn [md5-bytes]
+      (apply str (map (partial format "%02x") md5-bytes)))))
+
+
+; https://springrts.com/wiki/Rapid#an_index_archive:_.sdp
+(def sdp-line
+  (b/header
+    (b/ordered-map
+      :filename-length :ubyte)
+    (fn [{:keys [filename-length]}]
+      (b/ordered-map
+        ;:filename (b/blob :length filename-length)
+        :filename (b/string "ISO-8859-1" :length filename-length)
+        :md5 md5-codec
+        ;:md5 (b/string "UTF-8" :length 16)
+        :crc32 :uint-le ; (b/string "ISO-8859-1" :length 4)
+        :file-size :uint-le))
+    (constantly nil) ; TODO writing SDP files
+    :keep-header? false))
+
+
+(defn decode-sdp [f]
+  (with-open [is (io/input-stream f)
+              gz (GZIPInputStream. is)]
+    {::source (.getAbsolutePath f)
+     :items (b/decode (b/repeated sdp-line) gz)}))
+
+(defn sdp-files []
+  (let [packages-root (io/file (fs/spring-root) "packages")]
+    (if (.exists packages-root)
+      (.listFiles packages-root)
+      [])))
+
+(defn slurp-from-pool [md5]
+  (let [pool-dir (subs md5 0 2)
+        pool-file (str (subs md5 2) ".gz")
+        f (io/file (fs/spring-root) "pool" pool-dir pool-file)]
+    (with-open [is (io/input-stream f)
+                gz (GZIPInputStream. is)]
+      (slurp gz))))
+
+(defn inner [decoded-sdp inner-filename]
+  (if-let [inner-details (->> decoded-sdp
+                              :items
+                              (filter (comp #{inner-filename} :filename))
+                              first)]
+    (assoc inner-details :contents (slurp-from-pool (:md5 inner-details)))
+    (throw (ex-info "No such inner rapid file" {:package (::source decode-sdp)
+                                                :inner-filename inner-filename}))))
+
+(defn rapid-inner [sdp-file inner-filename]
+  (inner (decode-sdp sdp-file) inner-filename))
+
+#_
+(let [sdps (sdp-files)
+      one (->> sdps
+               (filter #(string/starts-with? (.getName %) "e9"))
+               first)
+      decoded (decode-sdp one)]
+  (println (::source decoded))
+  (rapid-inner one "modinfo.lua")
+  #_
+  (->> (decode-sdp one)
+       :items
+       (filter (comp #(string/starts-with? % "mod") :filename))
+       #_
+       (fn [{:keys [body]}]
+         (let [{:keys [md5]} body]
+           {:raw md5
+            :hex (format "%16x" (java.math.BigInteger. 1 md5))})))
+       ;:md5
+       ;detect)
+  #_
+  (b/decode sdp-line (io/input-stream one))
+  #_
+  (->> (io/input-stream one)
+       (b/decode :ubyte)
+       #_(b/decode sdp-line))
+  #_
+  (->> sdps
+       (map
+         (fn [f]
+           [(.getName f)
+            (decode-sdp (io/input-stream f))]))
+       (into {})))
