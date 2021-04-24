21b1905b834e9edc1251ac89bd53ddc4d5084ada
skynet <skynet-gh@users.noreply.github.com>
2021-04-24 09:31:48 -0400

    Add basic sanity test for welcome page

--- test/clj/skylobby/fx/welcome_test.clj
+++ test/clj/skylobby/fx/welcome_test.clj
@@ -4,6 +4,20 @@
     [skylobby.fx.welcome :as fx]))
 
 
+(deftest connect-button
+  (is (map?
+        (fx/connect-button nil))))
+
+(deftest singleplayer-buttons
+  (is (map?
+        (fx/singleplayer-buttons nil))))
+
+(deftest multiplayer-buttons
+  (is (map?
+        (fx/multiplayer-buttons nil))))
+
 (deftest welcome-view
   (is (map?
-        (fx/welcome-view nil))))
+        (fx/welcome-view nil)))
+  (is (map?
+        (fx/welcome-view {:by-server {:local {:battle {:battle-id :singleplayer}}}}))))
