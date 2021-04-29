1bfdc07506dbbfb796b642ec03b3fec6b67f673f
skynet <skynet-gh@users.noreply.github.com>
2021-04-29 18:53:18 -0400

    Fix country flags flashing

--- src/clj/skylobby/fx/flag_icon.clj
+++ src/clj/skylobby/fx/flag_icon.clj
@@ -8,13 +8,21 @@
 (set! *warn-on-reflection* true)
 
 
+(defn flag-image [country-code]
+  (try
+    (FlagIcon. country-code)
+    (catch Exception e
+      (log/trace e "Error creating flag icon"))))
+
+(def flag-image-memoized
+  (memoize flag-image))
+
+
 (defn flag-icon [{:keys [^String country-code]}]
   (merge
     {:fx/type :label
      :text (str country-code)}
-    (when-let [image (try (FlagIcon. country-code)
-                          (catch Exception e
-                            (log/trace e "Error creating flag icon")))]
+    (when-let [image (flag-image-memoized country-code)]
       {:graphic
        {:fx/type :image-view
         :image image}})))
