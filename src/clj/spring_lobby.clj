0083af4454fa2a7760e569fc1e6eef1416be7a2b
skynet <276247+skynet-gh@users.noreply.github.com>
2021-09-10 17:08:28 -0400

    Improve performance of replays window render

--- src/clj/spring_lobby.clj
+++ src/clj/spring_lobby.clj
@@ -854,7 +854,7 @@
 (defn fix-selected-replay-watcher [_k state-atom old-state new-state]
   (tufte/profile {:dynamic? true
                   :id ::state-watcher}
-    (tufte/p :fix-selected-replay-watcher
+    (tufte/p :fix-replays-watcher
       (when (not= (fix-selected-replay-relevant-keys old-state)
                   (fix-selected-replay-relevant-keys new-state))
         (try
@@ -924,6 +924,118 @@
       (catch Exception e
         (log/error e "Error in :update-battle-status-sync state watcher")))))
 
+(defn- filter-replays-relevant-keys [state]
+  (select-keys
+    state
+    [:filter-replay :filter-replay-max-players :filter-replay-min-players :filter-replay-min-skill
+     :filter-replay-source :filter-replay-type :online-bar-replays :parsed-replays-by-path
+     :replays-filter-specs :replays-tags :replays-window-dedupe]))
+
+(defn filter-replays-watcher [_k state-atom old-state new-state]
+  (when
+    (or (not (:filtered-replays new-state))
+        (and (not= old-state new-state)
+             (not= (filter-replays-relevant-keys old-state)
+                   (filter-replays-relevant-keys new-state))))
+    (try
+      (let [{:keys [filter-replay filter-replay-max-players filter-replay-min-players
+                    filter-replay-min-skill filter-replay-source filter-replay-type
+                    online-bar-replays parsed-replays-by-path replays-filter-specs replays-tags
+                    replays-window-dedupe]} new-state
+            local-filenames (->> parsed-replays-by-path
+                                 vals
+                                 (map :filename)
+                                 (filter some?)
+                                 set)
+            online-only-replays (->> online-bar-replays
+                                     vals
+                                     (remove (comp local-filenames :filename)))
+            all-replays (->> parsed-replays-by-path
+                             vals
+                             (concat online-only-replays)
+                             (sort-by (comp str :unix-time :header))
+                             reverse
+                             doall)
+            filter-terms (->> (string/split (or filter-replay "") #"\s+")
+                              (remove string/blank?)
+                              (map string/lower-case))
+            includes-term? (fn [s term]
+                             (let [lc (string/lower-case (or s ""))]
+                               (string/includes? lc term)))
+            replays (->> all-replays
+                         (filter
+                           (fn [replay]
+                             (if filter-replay-source
+                               (= filter-replay-source (:source-name replay))
+                               true)))
+                         (filter
+                           (fn [replay]
+                             (if filter-replay-type
+                               (= filter-replay-type (:game-type replay))
+                               true)))
+                         (filter
+                           (fn [replay]
+                             (if filter-replay-min-players
+                               (<= filter-replay-min-players (fx.replay/replay-player-count replay))
+                               true)))
+                         (filter
+                           (fn [replay]
+                             (if filter-replay-max-players
+                               (<= (fx.replay/replay-player-count replay) filter-replay-max-players)
+                               true)))
+                         (filter
+                           (fn [replay]
+                             (if filter-replay-min-skill
+                               (if-let [avg (fx.replay/average-skill (fx.replay/replay-skills replay))]
+                                 (<= filter-replay-min-skill avg)
+                                 false)
+                               true)))
+                         (filter
+                           (fn [replay]
+                             (if (empty? filter-terms)
+                               true
+                               (every?
+                                 (some-fn
+                                   (partial includes-term? (fx.replay/replay-id replay))
+                                   (partial includes-term? (get replays-tags (fx.replay/replay-id replay)))
+                                   (partial includes-term? (-> replay :header :engine-version))
+                                   (partial includes-term? (-> replay :body :script-data :game :gametype))
+                                   (partial includes-term? (-> replay :body :script-data :game :mapname))
+                                   (fn [term]
+                                     (let [players (some->> replay :body :script-data :game
+                                                            (filter (comp #(string/starts-with? % "player") name first))
+                                                            (filter
+                                                              (if replays-filter-specs
+                                                                (constantly true)
+                                                                (some-fn
+                                                                  (comp #{0 "0"} :spectator second)
+                                                                  (comp not #(contains? % :spectator) second))))
+                                                            (map (comp #(some % [:name :username]) second))
+                                                            (map fx.replay/sanitize-replay-filter))]
+                                       (some #(includes-term? % term) players))))
+                                 filter-terms))))
+                         doall)
+            replays (if replays-window-dedupe
+                      (:replays
+                        (reduce ; dedupe by id
+                          (fn [agg curr]
+                            (let [id (fx.replay/replay-id curr)]
+                              (cond
+                                (not id)
+                                (update agg :replays conj curr)
+                                (contains? (:seen-ids agg) id) agg
+                                :else
+                                (-> agg
+                                    (update :replays conj curr)
+                                    (update :seen-ids conj id)))))
+                          {:replays []
+                           :seen-ids #{}}
+                          replays))
+                      replays)]
+        (swap! state-atom assoc :filtered-replays replays))
+      (catch Exception e
+        (log/error e "Error in :filter-replays state watcher")))))
+
 
 (defn- add-watchers
   "Adds all *state watchers."
@@ -939,6 +1051,7 @@
   (remove-watch state-atom :auto-get-resources)
   (remove-watch state-atom :fix-selected-replay)
   (remove-watch state-atom :fix-selected-server)
+  (remove-watch state-atom :filter-replays)
   (remove-watch state-atom :update-battle-status-sync)
   #_
   (add-watch state-atom :battle-map-details
@@ -983,6 +1096,7 @@
                       :id ::state-watcher}
         (tufte/p :auto-get-resources-watcher
           (auto-get-resources-watcher _k state-atom old-state new-state)))))
+  (add-watch state-atom :filter-replays filter-replays-watcher)
   (add-watch state-atom :fix-selected-replay fix-selected-replay-watcher)
   (add-watch state-atom :fix-selected-server fix-selected-server-watcher)
   #_
