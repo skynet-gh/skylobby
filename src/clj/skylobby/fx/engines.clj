d13e197010c0e6d4c9b6407867d46bc195ac69c7
skynet <276247+skynet-gh@users.noreply.github.com>
2021-09-19 13:56:14 -0400

    Fix centering in resource pickers

--- src/clj/skylobby/fx/engines.clj
+++ src/clj/skylobby/fx/engines.clj
@@ -16,85 +16,86 @@
   [{:keys [downloadables-by-url engine-filter engine-version engines flow http-download on-value-changed
            spring-isolation-dir suggest tasks-by-type]
     :or {flow true}}]
-  {:fx/type (if flow :flow-pane :h-box)
-   ;:alignment :center-left
-   :children
-   (concat
-     [{:fx/type :label
-       :text " Engine: "}]
-     (if (empty? engines)
-       (if suggest
-         (let [downloadables (vals downloadables-by-url)]
-           (mapv
-             (fn [engine-version]
-               (let [downloadable (->> downloadables
-                                       (filter (partial resource/could-be-this-engine? engine-version))
-                                       first)
-                     download (get http-download (:download-url downloadable))
-                     running (:running download)
-                     task (->> (get tasks-by-type :spring-lobby/download-and-extract)
-                               (filter (comp (partial resource/same-resource-filename? downloadable) :downloadable))
-                               seq)]
-                 {:fx/type :button
-                  :text (cond
-                          running (u/download-progress download)
-                          task "Queued..."
-                          :else
-                          (str "Get " engine-version))
-                  :disable (boolean (or (not downloadable) running task))
-                  :on-action {:event/type :spring-lobby/add-task
-                              :task {:spring-lobby/task-type :spring-lobby/download-and-extract
-                                     :downloadable downloadable
-                                     :spring-isolation-dir spring-isolation-dir}}}))
-             known-engine-versions))
-        [{:fx/type :label
-          :text " No engines "}])
-      (let [filter-lc (if engine-filter (string/lower-case engine-filter) "")
-            filtered-engines (->> engines
-                                  (map :engine-version)
-                                  (filter some?)
-                                  (filter #(string/includes? (string/lower-case %) filter-lc))
-                                  sort)]
-        [{:fx/type :combo-box
-          :prompt-text " < pick an engine > "
-          :value engine-version
-          :items filtered-engines
-          :on-value-changed (or on-value-changed
-                                {:event/type :spring-lobby/assoc
-                                 :key :engine-version})
-          :cell-factory
-          {:fx/cell-type :list-cell
-           :describe
-           (fn [engine]
-             {:text (if (string/blank? engine)
-                      "< choose an engine >"
-                      engine)})}
-          :on-key-pressed {:event/type :spring-lobby/engines-key-pressed}
-          :on-hidden {:event/type :spring-lobby/dissoc
-                      :key :engine-filter}}]))
-     [{:fx/type fx.ext.node/with-tooltip-props
-       :props
-       {:tooltip
-        {:fx/type :tooltip
-         :show-delay [10 :ms]
-         :text "Open engine directory"}}
-       :desc
-       {:fx/type :button
-        :on-action {:event/type :spring-lobby/desktop-browse-dir
-                    :file (io/file spring-isolation-dir "engine")}
-        :graphic
-        {:fx/type font-icon/lifecycle
-         :icon-literal "mdi-folder:16:white"}}}
-      {:fx/type fx.ext.node/with-tooltip-props
-       :props
-       {:tooltip
-        {:fx/type :tooltip
-         :show-delay [10 :ms]
-         :text "Reload engines"}}
-       :desc
-       {:fx/type :button
-        :on-action {:event/type :spring-lobby/add-task
-                    :task {:spring-lobby/task-type :spring-lobby/refresh-engines}}
-        :graphic
-        {:fx/type font-icon/lifecycle
-         :icon-literal "mdi-refresh:16:white"}}}])})
+  (merge
+    {:fx/type (if flow :flow-pane :h-box)}
+    (when-not flow {:alignment :center-left})
+    {:children
+     (concat
+       [{:fx/type :label
+         :text " Engine: "}]
+       (if (empty? engines)
+         (if suggest
+           (let [downloadables (vals downloadables-by-url)]
+             (mapv
+               (fn [engine-version]
+                 (let [downloadable (->> downloadables
+                                         (filter (partial resource/could-be-this-engine? engine-version))
+                                         first)
+                       download (get http-download (:download-url downloadable))
+                       running (:running download)
+                       task (->> (get tasks-by-type :spring-lobby/download-and-extract)
+                                 (filter (comp (partial resource/same-resource-filename? downloadable) :downloadable))
+                                 seq)]
+                   {:fx/type :button
+                    :text (cond
+                            running (u/download-progress download)
+                            task "Queued..."
+                            :else
+                            (str "Get " engine-version))
+                    :disable (boolean (or (not downloadable) running task))
+                    :on-action {:event/type :spring-lobby/add-task
+                                :task {:spring-lobby/task-type :spring-lobby/download-and-extract
+                                       :downloadable downloadable
+                                       :spring-isolation-dir spring-isolation-dir}}}))
+               known-engine-versions))
+          [{:fx/type :label
+            :text " No engines "}])
+        (let [filter-lc (if engine-filter (string/lower-case engine-filter) "")
+              filtered-engines (->> engines
+                                    (map :engine-version)
+                                    (filter some?)
+                                    (filter #(string/includes? (string/lower-case %) filter-lc))
+                                    sort)]
+          [{:fx/type :combo-box
+            :prompt-text " < pick an engine > "
+            :value engine-version
+            :items filtered-engines
+            :on-value-changed (or on-value-changed
+                                  {:event/type :spring-lobby/assoc
+                                   :key :engine-version})
+            :cell-factory
+            {:fx/cell-type :list-cell
+             :describe
+             (fn [engine]
+               {:text (if (string/blank? engine)
+                        "< choose an engine >"
+                        engine)})}
+            :on-key-pressed {:event/type :spring-lobby/engines-key-pressed}
+            :on-hidden {:event/type :spring-lobby/dissoc
+                        :key :engine-filter}}]))
+       [{:fx/type fx.ext.node/with-tooltip-props
+         :props
+         {:tooltip
+          {:fx/type :tooltip
+           :show-delay [10 :ms]
+           :text "Open engine directory"}}
+         :desc
+         {:fx/type :button
+          :on-action {:event/type :spring-lobby/desktop-browse-dir
+                      :file (io/file spring-isolation-dir "engine")}
+          :graphic
+          {:fx/type font-icon/lifecycle
+           :icon-literal "mdi-folder:16:white"}}}
+        {:fx/type fx.ext.node/with-tooltip-props
+         :props
+         {:tooltip
+          {:fx/type :tooltip
+           :show-delay [10 :ms]
+           :text "Reload engines"}}
+         :desc
+         {:fx/type :button
+          :on-action {:event/type :spring-lobby/add-task
+                      :task {:spring-lobby/task-type :spring-lobby/refresh-engines}}
+          :graphic
+          {:fx/type font-icon/lifecycle
+           :icon-literal "mdi-refresh:16:white"}}}])}))
--- src/clj/skylobby/fx/mods.clj
+++ src/clj/skylobby/fx/mods.clj
@@ -27,118 +27,119 @@
            rapid-data-by-id rapid-download spring-isolation-dir suggest tasks-by-type]
     :or {flow true}}]
   (let [games (filter :is-game mods)]
-    {:fx/type (if flow :flow-pane :h-box)
-     ;:alignment :center-left
-     :children
-     (concat
-       [{:fx/type :label
-         :alignment :center-left
-         :text " Game: "}]
-       (if (empty? games)
-         (if suggest
-           [{:fx/type :flow-pane
-             :children
-             (let [downloadables (vals downloadables-by-url)]
-               (map
-                 (fn [{:keys [mod-name source version]}]
-                   (let [downloadable (case source
-                                        :rapid (:id (get rapid-data-by-id version))
-                                        :http (->> downloadables
-                                                   (filter (partial resource/could-be-this-mod? version))
-                                                   first)
-                                        nil)
-                         on-action (case source
-                                     :rapid
-                                     {:event/type :spring-lobby/add-task
-                                      :task
-                                      (if downloadable
-                                        {:spring-lobby/task-type :spring-lobby/rapid-download
-                                         :rapid-id version
-                                         :engine-file engine-file
-                                         :spring-isolation-dir spring-isolation-dir}
-                                        {:spring-lobby/task-type :spring-lobby/update-rapid
-                                         :force true})}
-                                     :http
-                                     {:event/type :spring-lobby/add-task
-                                      :task {:spring-lobby/task-type :spring-lobby/http-downloadable
-                                             :downloadable downloadable
-                                             :spring-isolation-dir spring-isolation-dir}}
-                                     nil)
-                         download (or (get http-download (:download-url downloadable))
-                                      (get rapid-download downloadable))
-                         running (:running download)
-                         task (seq
-                                (concat
-                                  (filter
-                                    (comp #{downloadable} :id)
-                                    (get tasks-by-type :spring-lobby/rapid-download))
-                                  (filter
-                                    (comp (partial resource/same-resource-filename? downloadable) :downloadable)
-                                    (get tasks-by-type :spring-lobby/http-downloadable))))]
-                     {:fx/type :button
-                      :text (if (and (not downloadable) (= :rapid source))
-                              "Update rapid"
-                              (cond
-                                running (u/download-progress download)
-                                task "Queued..."
-                                :else
-                                (str "Get " mod-name)))
-                      :disable (boolean
-                                 (or
-                                   (and (not downloadable)
-                                        (not= :rapid source))
-                                   running
-                                   task))
-                      :on-action on-action}))
-                 known-games))}]
-           [{:fx/type :label
-             :text " No games"}])
-         (let [filter-lc (if mod-filter (string/lower-case mod-filter) "")
-               filtered-games (->> games
-                                   (map :mod-name)
-                                   (filter string?)
-                                   (filter #(string/includes? (string/lower-case %) filter-lc))
-                                   (sort version/version-compare))]
-           [{:fx/type :combo-box
-             :prompt-text " < pick a game > "
-             :value mod-name
-             :items filtered-games
-             :on-value-changed (or on-value-changed
-                                   {:event/type :spring-lobby/assoc
-                                    :key :mod-name})
-             :cell-factory
-             {:fx/cell-type :list-cell
-              :describe
-              (fn [mod-name]
-                {:text (if (string/blank? mod-name)
-                         "< choose a game >"
-                         mod-name)})}
-             :on-key-pressed {:event/type :spring-lobby/mods-key-pressed}
-             :on-hidden {:event/type :spring-lobby/dissoc
-                         :key :mod-filter}}]))
-       [{:fx/type fx.ext.node/with-tooltip-props
-         :props
-         {:tooltip
-          {:fx/type :tooltip
-           :show-delay [10 :ms]
-           :text "Open games directory"}}
-         :desc
-         {:fx/type :button
-          :on-action {:event/type :spring-lobby/desktop-browse-dir
-                      :file (io/file spring-isolation-dir "games")}
-          :graphic
-          {:fx/type font-icon/lifecycle
-           :icon-literal "mdi-folder:16:white"}}}
-        {:fx/type fx.ext.node/with-tooltip-props
-         :props
-         {:tooltip
-          {:fx/type :tooltip
-           :show-delay [10 :ms]
-           :text "Reload games"}}
-         :desc
-         {:fx/type :button
-          :on-action {:event/type :spring-lobby/add-task
-                      :task {:spring-lobby/task-type :spring-lobby/refresh-mods}}
-          :graphic
-          {:fx/type font-icon/lifecycle
-           :icon-literal "mdi-refresh:16:white"}}}])}))
+    (merge
+      {:fx/type (if flow :flow-pane :h-box)}
+      (when-not flow {:alignment :center-left})
+      {:children
+       (concat
+         [{:fx/type :label
+           :alignment :center-left
+           :text " Game: "}]
+         (if (empty? games)
+           (if suggest
+             [{:fx/type :flow-pane
+               :children
+               (let [downloadables (vals downloadables-by-url)]
+                 (map
+                   (fn [{:keys [mod-name source version]}]
+                     (let [downloadable (case source
+                                          :rapid (:id (get rapid-data-by-id version))
+                                          :http (->> downloadables
+                                                     (filter (partial resource/could-be-this-mod? version))
+                                                     first)
+                                          nil)
+                           on-action (case source
+                                       :rapid
+                                       {:event/type :spring-lobby/add-task
+                                        :task
+                                        (if downloadable
+                                          {:spring-lobby/task-type :spring-lobby/rapid-download
+                                           :rapid-id version
+                                           :engine-file engine-file
+                                           :spring-isolation-dir spring-isolation-dir}
+                                          {:spring-lobby/task-type :spring-lobby/update-rapid
+                                           :force true})}
+                                       :http
+                                       {:event/type :spring-lobby/add-task
+                                        :task {:spring-lobby/task-type :spring-lobby/http-downloadable
+                                               :downloadable downloadable
+                                               :spring-isolation-dir spring-isolation-dir}}
+                                       nil)
+                           download (or (get http-download (:download-url downloadable))
+                                        (get rapid-download downloadable))
+                           running (:running download)
+                           task (seq
+                                  (concat
+                                    (filter
+                                      (comp #{downloadable} :id)
+                                      (get tasks-by-type :spring-lobby/rapid-download))
+                                    (filter
+                                      (comp (partial resource/same-resource-filename? downloadable) :downloadable)
+                                      (get tasks-by-type :spring-lobby/http-downloadable))))]
+                       {:fx/type :button
+                        :text (if (and (not downloadable) (= :rapid source))
+                                "Update rapid"
+                                (cond
+                                  running (u/download-progress download)
+                                  task "Queued..."
+                                  :else
+                                  (str "Get " mod-name)))
+                        :disable (boolean
+                                   (or
+                                     (and (not downloadable)
+                                          (not= :rapid source))
+                                     running
+                                     task))
+                        :on-action on-action}))
+                   known-games))}]
+             [{:fx/type :label
+               :text " No games"}])
+           (let [filter-lc (if mod-filter (string/lower-case mod-filter) "")
+                 filtered-games (->> games
+                                     (map :mod-name)
+                                     (filter string?)
+                                     (filter #(string/includes? (string/lower-case %) filter-lc))
+                                     (sort version/version-compare))]
+             [{:fx/type :combo-box
+               :prompt-text " < pick a game > "
+               :value mod-name
+               :items filtered-games
+               :on-value-changed (or on-value-changed
+                                     {:event/type :spring-lobby/assoc
+                                      :key :mod-name})
+               :cell-factory
+               {:fx/cell-type :list-cell
+                :describe
+                (fn [mod-name]
+                  {:text (if (string/blank? mod-name)
+                           "< choose a game >"
+                           mod-name)})}
+               :on-key-pressed {:event/type :spring-lobby/mods-key-pressed}
+               :on-hidden {:event/type :spring-lobby/dissoc
+                           :key :mod-filter}}]))
+         [{:fx/type fx.ext.node/with-tooltip-props
+           :props
+           {:tooltip
+            {:fx/type :tooltip
+             :show-delay [10 :ms]
+             :text "Open games directory"}}
+           :desc
+           {:fx/type :button
+            :on-action {:event/type :spring-lobby/desktop-browse-dir
+                        :file (io/file spring-isolation-dir "games")}
+            :graphic
+            {:fx/type font-icon/lifecycle
+             :icon-literal "mdi-folder:16:white"}}}
+          {:fx/type fx.ext.node/with-tooltip-props
+           :props
+           {:tooltip
+            {:fx/type :tooltip
+             :show-delay [10 :ms]
+             :text "Reload games"}}
+           :desc
+           {:fx/type :button
+            :on-action {:event/type :spring-lobby/add-task
+                        :task {:spring-lobby/task-type :spring-lobby/refresh-mods}}
+            :graphic
+            {:fx/type font-icon/lifecycle
+             :icon-literal "mdi-refresh:16:white"}}}])})))
