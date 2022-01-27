654a3018b8e3ec8df53689b0c6de8113b4aff90b
skynet <skynet-gh@users.noreply.github.com>
2022-01-26 19:20:54 -0500

    Default windows to center instead of zero

--- test/clj/skylobby/fx_test.clj
+++ test/clj/skylobby/fx_test.clj
@@ -10,9 +10,9 @@
 (def screen-bounds
   {:min-x -2560.0
    :min-y 0.0
-   :max-x 2498.0
+   :max-x 2560.0
    :max-y 1440.0
-   :width 5058.0
+   :width 5120.0
    :height 1440.0})
 
 
@@ -20,19 +20,19 @@
   (is (= 0
          (with-redefs [skylobby.fx/get-screen-bounds (constantly nil)]
            (skylobby.fx/fitx nil))))
-  (is (= -2560.0
+  (is (= 0.0
          (with-redefs [skylobby.fx/get-screen-bounds (constantly screen-bounds)]
            (skylobby.fx/fitx nil))))
-  (is (= -2560.0
+  (is (= 0.0
          (skylobby.fx/fitx screen-bounds))))
 (deftest fity
   (is (= 0
          (with-redefs [skylobby.fx/get-screen-bounds (constantly nil)]
            (skylobby.fx/fity nil))))
-  (is (= 0.0
+  (is (= 720.0
          (with-redefs [skylobby.fx/get-screen-bounds (constantly screen-bounds)]
            (skylobby.fx/fity nil))))
-  (is (= 0.0
+  (is (= 720.0
          (skylobby.fx/fity screen-bounds))))
 (deftest fitwidth
   (is (= 256
