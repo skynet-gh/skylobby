75e7e9c254dd54a86fd45c65681ad536da973771
skynet <skynet-gh@users.noreply.github.com>
2022-04-02 13:22:22 -0400

    fix helper script

--- update-native-res-win.ps1
+++ update-native-res-win.ps1
@@ -1,2 +1,2 @@
 
-& "$env:GRAALVM_HOME\bin\java" -javaagent:clojure-native-image-agent.jar=initialize-class=skylobby.main,output-dir=native-res\windows\META-INF\native-image\skylobby -agentlib:native-image-agent=config-merge-dir=native-res\windows\META-INF\native-image\skylobby -jar target\skylobby.jar %*
+& "$env:GRAALVM_HOME\bin\java" -javaagent:clojure-native-image-agent.jar=initialize-class=skylobby.main,output-dir=native-res\windows\META-INF\native-image\skylobby -agentlib:native-image-agent=config-merge-dir=native-res\windows\META-INF\native-image\skylobby -jar target\skylobby.jar $args
