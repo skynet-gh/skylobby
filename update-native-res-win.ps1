6b5a4a40d0adac85f575c6006ac712a42240275a
skynet <skynet-gh@users.noreply.github.com>
2022-04-02 17:58:06 -0400

    Update native res

--- update-native-res-win.ps1
+++ update-native-res-win.ps1
@@ -1,2 +1,2 @@
 
-& "$env:GRAALVM_HOME\bin\java" -javaagent:clojure-native-image-agent.jar=initialize-class=skylobby.main,output-dir=native-res\windows\META-INF\native-image\skylobby -agentlib:native-image-agent=config-merge-dir=native-res\windows\META-INF\native-image\skylobby -jar target\skylobby.jar $args
+& "$env:GRAALVM_HOME\bin\java" -javaagent:clojure-native-image-agent.jar=initialize-class=skylobby.main,output-dir=native-res\windows\META-INF\native-image\skylobby -agentlib:native-image-agent=config-merge-dir=native-res\windows\META-INF\native-image\skylobby,config-write-period-secs=5 -jar target\skylobby.jar $args
