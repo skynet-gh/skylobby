ba0bf1ca683368efda6d41b16789e0dfc407fe7f
skynet <skynet-gh@users.noreply.github.com>
2022-10-20 20:15:22 -0400

    Remove clj-kondo hooks from git

--- .clj-kondo/clj-commons/aleph/aleph/clj_kondo_hooks.clj
+++
@@ -1,6 +0,0 @@
-(ns aleph.clj-kondo-hooks)
-
-(defmacro def-http-method [method]
-  `(defn ~method
-     ([url#])
-     ([url# options#])))
--- .clj-kondo/cnuernber/dtype-next/tech/v3/datatype.clj
+++
@@ -1,9 +0,0 @@
-(ns tech.v3.datatype)
-
-(defmacro make-reader
-  ([datatype n-elems read-op]
-   `(let [~'idx ~n-elems]
-       ~read-op))
-  ([reader-datatype adv-datatype n-elems read-op]
-   `(let [~'idx ~n-elems]
-      ~read-op)))
